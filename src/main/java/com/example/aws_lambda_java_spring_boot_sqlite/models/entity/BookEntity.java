a7a7aa12d8c929ba171ef2097a093a777d781f97
jacobhrussell <jacobhenryrussell@gmail.com>
2024-11-17 17:31:12 -0500

    adding flyway config

--- src/main/java/com/example/aws_lambda_java_spring_boot_sqlite/models/entity/BookEntity.java
+++ src/main/java/com/example/aws_lambda_java_spring_boot_sqlite/models/entity/BookEntity.java
@@ -10,4 +10,6 @@ public class BookEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String title;
+
 }
---
+++ src/main/resources/db/migration/V1.0__create_book_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE book
+(
+    id    INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL
+);
